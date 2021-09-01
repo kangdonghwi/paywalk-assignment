6ccead56cd64ddefed4f0a37fa8113b794a2fe0e
KANGDONGHWI <rkdehdgnl@naver.com>
2021-09-02 00:19:50 +0900

    feat: redux-saga 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import { rootReducer } from 'reducers';
+import rootSaga from 'saga';
+import App from './App';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import App from './App';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(rootReducer, composeWithDevTools());
-
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <App />
