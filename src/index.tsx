0e608127218fa98d3842dd9af1387a02949f9ace
KANGDONGHWI <rkdehdgnl@naver.com>
2021-09-01 17:25:55 +0900

    feat: store 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { rootReducer } from 'reducers';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>,
+  document.getElementById('root'),
 );
